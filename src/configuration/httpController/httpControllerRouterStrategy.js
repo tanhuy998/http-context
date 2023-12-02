5e2769da4b42df158ff3e2fcfa228e925cfb3c7e
Huy Tran <tanhuy998@gmail.com>
2023-12-03 05:28:53 +0700

    remove unused codes

--- src/configuration/httpController/httpControllerRouterStrategy.js
+++ src/configuration/httpController/httpControllerRouterStrategy.js
@@ -1,19 +1,20 @@
 module.exports = class HttpControllerRouterStrategy {
 
+
     /**
      * @description
-     * INDEPENDENT indicates that a specific controller class is registered via the HttpContext class
-     * and is managed by the http context over it's pipeline.
+     * DEPENDENT indicates that the a specified cotroller class will immediately serve incomming
+     * request as an expess handler.
      */
     static get DEPENDENT() {
 
         return 1;
     }
 
-    /**
+        /**
      * @description
-     * DEPENDENT indicates that the a specified cotroller class will immediately serve incomming
-     * request as an expess handler.
+     * INDEPENDENT indicates that a specific controller class is registered via the HttpContext class
+     * and is managed by the http context over it's pipeline.
      */
     static get INDEPENDENT() {
 
--- src/controller/httpController.js
+++ src/controller/httpController.js
@@ -1,17 +1,10 @@
 const BaseController = require("./baseController");
-const express = require('express');
-const {getRegisteredMethods, getRoute} = require('../utils/route/httpRoute.js');
 const RouteMetadata = require("../utils/route/routeMetadata.js");
 const self = require('reflectype/src/utils/self.js');
-const {convertToVerbList} = require('../utils/httpMethodEncoding.js');
 const RouteMap = require("../utils/route/routeMap.js");
-
-const {generateExpressHandler, generateInternalHandler} = require('../expressHandler.js');
 const { hasControllerMetadata } = require("../utils/requestMetadata.js");
-const { getControllerRouteGroup } = require("../utils/route/route.utils.js");
 const { BASE_HTTP_CONTROLLER, CONFIGURATION, SUB_CLASS_ID } = require("./constant.js");
 const HttpControllerConfiguration = require("../configuration/httpController/httpControllerConfiguration.js");
-const HttpControllerRouterStrategy = require("../configuration/httpController/httpControllerRouterStrategy.js");
 const HttpControllerConfigurator = require("../configuration/httpController/httpControllerConfigurator.js");
 
 /**
@@ -87,142 +80,6 @@ const proto = module.exports = class HttpController extends BaseController {
 
         return this.configuration.getIndependentRouter(this);
     }
-
-    // /**
-    //  * For static call handling without pipeline
-    //  * 
-    //  * @type {express.Router}
-    //  */
-    // static expressRouter = undefined;
-
-    // static featureRouter;
-
-    // /**
-    //  * For handling inside a pipeline
-    //  */
-    // /**@type {express.Router} */
-    // static internalRouter = undefined;
-
-    // /**@type {RouteMap} */
-    // static routeMap;
-
-    // /**@type {Symbol} */
-    // static perSubClassId;
-
-    // static routeRegex;
-
-    // static get filterRouter() {
-
-    //     if (!this.internalRouter) {
-
-    //         this._init();
-    //     }
-        
-    //     const routeGroup = getControllerRouteGroup(this);
-
-    //     return routeGroup?.mountGroup(this.internalRouter) ?? this.internalRouter;
-    // }
-
-    // static get router() {
-
-    //     if (!this.expressRouter) {
-
-    //         this._init();
-    //     }
-
-    //     const routeGroup = getControllerRouteGroup(this);
-
-    //     return routeGroup?.mountGroup(this.expressRouter) ?? this.expressRouter;
-    // }
-
-    // static get feature() {
-
-    //     if (!this.featureRouter) {
-
-    //         this._init();
-    //     }
-
-    //     return this.featureRouter;
-    // }
-
-    // static serve() {
-
-    //    return this.router;
-    // }
-
-    // static _init() {
-        
-    //     if (this.expressRouter !== undefined || this.internalRouter !== undefined) {
-
-    //         return;
-    //     }
-
-    //     this.expressRouter = express.Router();
-
-    //     this.internalRouter = express.Router();
-
-    //     this.featureRouter = {
-    //         before: express.Router(),
-    //         after: express.Router()
-    //     };
-
-    //     //this.id = typeof this.id === 'symbol' ? this.id : Symbol(this.name);
-
-    //     this._initPerSubClassId();
-
-    //     this.initRouteMap();
-
-    //     this.registerRoutes();
-    // }
-
-    // static initRouteMap() {
-
-    //     this.routeMap = new RouteMap();
-    // }
-
-    // /**
-    //  * setups route that declared on each method except route group
-    //  * 
-    //  * @returns 
-    //  */
-    // static registerRoutes() {
-
-    //     const exprRouter = this.expressRouter;
-    //     const internalRouter = this.internalRouter;
-    //     const routeMap = this.routeMap;
-
-    //     if (!exprRouter) {
-
-    //         return;
-    //     }
-
-    //     const methods = getRegisteredMethods(this);
-        
-    //     for (const fn of methods || []) {
-            
-    //         const route = getRoute(fn);
-
-    //         for (const entry of route.all.entries()) {
-                
-    //             const [pattern, verbChain] = entry;
-                
-    //             if (typeof pattern !== 'string' || typeof verbChain !== 'number') {
-
-    //                 continue;
-    //             }
-
-    //             routeMap.map(pattern, route);                
-
-    //             const verbList = convertToVerbList(verbChain);
-                
-    //             for (const verb of verbList || []) {
-                    
-    //                 exprRouter[verb](pattern, generateExpressHandler(this, pattern));
-    //                 internalRouter[verb](pattern, generateInternalHandler(this, pattern));
-    //             }               
-    //         }
-    //     }
-    // }
 
     /**
      * PROTOTYPE AREA
